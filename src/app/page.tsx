e9802c7acf091590666f61cf613a85b64063e642
Juan Camilo Echeverri Salazar <juancami98.11@hotmail.com>
2024-10-13 12:42:37 -0500

    Refactor font configuration and add new font
    - Refactor the font configuration in `fonts.ts` to include the `Poppins` font from Google Fonts.
    - Update the `geistAlmarai` variable to include the `Poppins` font subset for Latin characters.
    - Add the `geistPoppins` variable to configure the `Poppins` font with different weights and subsets.
    
    Add TypewriterComponent to HeroComponent
    
    - Create a new file `HeroComponent.tsx` in the `src/components/home` directory.
    - Import the `TypewriterComponent` from the `typewriter-effect` library.
    - Add a hero section to display a dynamic typewriter effect with different phrases.
    - Include a registration button and an image in the hero section.
    
    Update global CSS styles
    
    - Update the font family for heading elements (`h1`, `h2`, `h3`) to use the `Poppins` font.
    - Adjust the color values for the `--border`, `--input`, and `--ring` CSS variables.
    
    Add Card component
    
    - Create a new file `card.tsx` in the `src/components/ui` directory.
    - Implement the `Card`, `CardHeader`, `CardTitle`, `CardDescription`, `CardContent`, and `CardFooter` components.
    - Style the components to have a card-like appearance with rounded corners, borders, and background color.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,19 @@
-import { Button } from "@/components/ui/button";
-import { MaxWidthWrapper } from "@/components/ui/maxWidthWrapper/MaxWidthWrapper";
-import Image from "next/image";
+import { FeatureSectionSteps } from '@/components/home/FeatureSectionSteps';
+import { HeroComponent } from '@/components/home/HeroComponent';
+import { MaxWidthWrapper } from '@/components/ui/maxWidthWrapper/MaxWidthWrapper';
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
-      <div className=" min-h-screen flex flex-col md:flex-row items-start justify-between  ">
-        <div className="w-full md:w-1/2 mb-8 md:mb-0 text-center md:text-left mt-40">
-          <h1 className="text-6xl font-bold mb-4">
-            Enterprise Standard for Private Gen AI and Document Processing
-          </h1>
-          <p className="mb-4 text-secondary-foreground/70">
-            The TitanML Enterprise Inference Stack makes building, deploying ands
-            scaling private and secure Enterprise
-            applications, effortless.{" "}
-          </p>
-          <Button
-            size="xl"
-            className="bg-primary text-xl text-primary-foreground"
-          >
-            Regístrate
-          </Button>
-        </div>
-        <div className="w-1/2">
-          <Image
-            src="/isometric-blue.png"
-            alt="Hero Image"
-            width={3000}
-            height={3000}
-          />
-        </div>
+      <div className=" min-h-screen flex flex-col md:flex-row items-start justify-between">
+        <HeroComponent />
       </div>
-      <div className="bg-green-100 py-16 text-center">
-        <h2 className="text-3xl font-bold mb-4">Pasos a Seguir</h2>
-        <p className="text-lg">Sigue estos sencillos pasos para comenzar.</p>
-      </div>
+      <FeatureSectionSteps />
       <div className="bg-yellow-100 py-16 text-center">
         <h2 className="text-3xl font-bold mb-4">Proyectos Disponibles</h2>
         <p className="text-lg">Explora los proyectos que tenemos para ti.</p>
       </div>
+
       <div className="bg-red-100 py-16 text-center">
         <h2 className="text-3xl font-bold mb-4">Quiénes Somos</h2>
         <p className="text-lg">Conoce más sobre nuestro equipo y misión.</p>
---
+++ src/components/home/FeatureSectionSteps.tsx
@@ -0,0 +1,100 @@
+import { BiMoneyWithdraw } from 'react-icons/bi';
+import { BsBuildingFillAdd } from 'react-icons/bs';
+import { FaUserPlus } from 'react-icons/fa';
+import { GiPayMoney } from 'react-icons/gi';
+import { PiMoneyFill } from 'react-icons/pi';
+import { Card, CardContent } from '../ui/card';
+
+const steps = [
+  {
+    number: 1,
+    title: 'Regístrate',
+    description: 'Proporciona tus datos personales, correo electrónico, número de teléfono, cédula y firma.',
+    icon: <FaUserPlus className=" text-primary" size={35} />,
+  },
+  {
+    number: 2,
+    title: 'Deposita tus fondos',
+    description:
+      'Ingresa la cantidad deseada a tu cuenta en la plataforma para comprar acciones de 200.000 COP cada una.',
+    icon: <PiMoneyFill className=" text-primary" size={35} />,
+  },
+  {
+    number: 3,
+    title: 'Elige la propiedad',
+    description:
+      'Selecciona la propiedad que mejor se ajuste a tu perfil de inversionista con la información proporcionada.',
+    icon: <BsBuildingFillAdd className=" text-primary" size={35} />,
+  },
+  {
+    number: 4,
+    title: 'Recibe tus retribuciones',
+    description:
+      'Recibe rentas mensuales por tus acciones y, al liquidar el inmueble, obtén tu inversión inicial más la valorización.',
+    icon: <GiPayMoney className=" text-primary" size={35} />,
+  },
+  {
+    number: 5,
+    title: 'Retira tu dinero',
+    description:
+      'Retira tus retribuciones a partir de 50.000 COP a tu cuenta bancaria o medio registrado, o reinviértelo en otras propiedades.',
+    icon: <BiMoneyWithdraw className="  text-primary" size={35} />,
+  },
+];
+
+export const FeatureSectionSteps = () => {
+  return (
+    <section>
+      <div className="py-8 mx-auto sm:py-16">
+        <div className="max-w-screen-md mb-8 lg:mb-16">
+          <h2 className="mb-4 text-5xl tracking-tight font-bold text-primary">Pasos para tu inversión</h2>
+          <p className="text-gray-600 sm:text-xl">
+            Sigue estos sencillos pasos para comenzar a invertir en bienes raíces y obtener retribuciones mensuales.
+          </p>
+        </div>
+        <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0 mb-10">
+          {steps.map((step, index) => {
+            if (steps.length % 3 === 2 && (index === steps.length - 2 || index === steps.length - 1)) {
+              return null;
+            }
+            return (
+              <Card key={step.number} className=" shadow-lg p-10">
+                <CardContent>
+                  <div className="flex flex-col items-center">
+                    <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary/20 lg:h-20 lg:w-20">
+                      <div className="">{step.icon}</div>
+                    </div>
+                    <h3 className="mb-2 text-xl font-bold text-primary">{step.title}</h3>
+                    <p className="text-gray-500">{step.description}</p>
+                  </div>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
+        {steps.length % 3 === 2 && (
+          <div className="flex justify-center lg:col-span-3 space-x-12 ">
+            <Card className=" shadow-lg p-10 w-full lg:w-1/3">
+              <CardContent className="flex flex-col items-center">
+                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary/20 lg:h-20 lg:w-20 ">
+                  <div className="">{steps[steps.length - 2].icon}</div>
+                </div>
+                <h3 className="mb-2 text-xl text-primary font-bold">{steps[steps.length - 2].title}</h3>
+                <p className="text-gray-500">{steps[steps.length - 2].description}</p>
+              </CardContent>
+            </Card>
+            <Card className=" shadow-lg p-10 w-full lg:w-1/3">
+              <CardContent className="flex flex-col items-center">
+                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary/20 lg:h-20 lg:w-20 ">
+                  <div className="">{steps[steps.length - 1].icon}</div>
+                </div>
+                <h3 className="mb-2 text-xl text-primary font-bold">{steps[steps.length - 1].title}</h3>
+                <p className="text-gray-500">{steps[steps.length - 1].description}</p>
+              </CardContent>
+            </Card>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
